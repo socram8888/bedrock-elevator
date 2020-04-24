db0cc59d7d31734020346372aaf22b15345fb423
Marcos Del Sol Vives <marcos@orca.pet>
2020-04-24 20:21:02 +0200

    Be extremely finicky with JS checking

--- test-syntax.sh
+++ test-syntax.sh
@@ -3,7 +3,7 @@
 cd "$(dirname "$0")"
 
 if which jshint &>/dev/null; then
-	find -name "*.json" -o -name "*.js" -print0 | xargs -0 jshint
+	find \( -name "*.json" -o -name "*.js" \) -print0 | xargs -0 jshint
 else
 	echo "JShint not installed - syntax cannot be checked"
 fi
